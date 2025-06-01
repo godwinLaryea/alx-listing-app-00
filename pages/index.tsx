26262f5691580a9d8da15cda0cfe7bcab3741f49
godwinLaryea <kalexgun@gmail.com>
2025-06-01 10:09:47 +0000

    minor change

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,25 @@
-import Layout from "@/components/layout/Layout";
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import Image from "next/image";
+import { myImages, IMAGE_PATHS } from "@/constants/images";
+import PropertyListing from "@/components/common/PropertyListing";
 
-export default function App({ Component, pageProps }: AppProps) {
+const Home = () => {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <main>
+      <section className="w-full min-h-screen">
+        <div className="hero-section p-[3em] md:py-[8em] lg:py-[14em] mx-4 md:mx-8 rounded-2xl">
+          <div className="text-center text-white max-w-[100%]">
+            <h1 className="text-3xl md:text-4xl lg:text-7xl text-center">
+              Find your favorite place here!
+            </h1>
+            <p className="text-xs md:text-lg lg:text-2xl">
+              The best prices for over 2 million properties worldwide
+            </p>
+          </div>
+        </div>
+        <PropertyListing />
+      </section>
+    </main>
   );
-}
+};
+
+export default Home;
